591bcfa5b7f72119a79a5aa0957c1ab1ca8f0246
Job <job.van.der.meiden@hva.nl>
2020-11-11 20:40:43 +0100

    update

--- plantenbak.py
+++ plantenbak.py
@@ -1,17 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 
-pumpPin = 18;
+pumpPin = 18
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(pumpPin, GPIO.OUT)
 
+
 def activatePump(duration):
-    print "Pump on."
+    print("Pump on.")
     GPIO.output(pumpPin, GPIO.HIGH)
     time.sleep(duration)
-    print "pump off"
+    print("pump off")
     GPIO.output(pumpPin, GPIO.LOW)
+
 
 activatePump(10)

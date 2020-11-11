fde728e1d4703100c165d2ad6b799dba8a672a1f
Job <job.van.der.meiden@hva.nl>
2020-11-11 21:39:20 +0100

    pin in test

--- plantenbak.py
+++ plantenbak.py
@@ -9,16 +9,18 @@ GPIO.setwarnings(False)
 GPIO.setup(pumpPin, GPIO.OUT)
 GPIO.setup(sensorPin, GPIO.IN)
 
-
 def activatePump(duration):
     print("Pump on.")
     GPIO.output(pumpPin, GPIO.LOW)
     for i in range(10):
-        time.sleep(duration / 10)
-        print(str(GPIO.input(sensorPin)))
-        if GPIO.input(sensorPin):
-            break
+       print(str(GPIO.input(sensorPin)))
+       time.sleep(duration / 10)
     print("pump off")
     GPIO.output(pumpPin, GPIO.HIGH)
 
+
 activatePump(10)
+
+while True:
+    print(str(GPIO.input(sensorPin)))
+    time.sleep(1)

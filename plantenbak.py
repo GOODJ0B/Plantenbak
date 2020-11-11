d149976d0aee784eb863ce48a4a50c896af769c2
Job <job.van.der.meiden@hva.nl>
2020-11-11 20:40:00 +0100

    added plantenbak

---
+++ plantenbak.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+pumpPin = 18;
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pumpPin, GPIO.OUT)
+
+def activatePump(duration):
+    print "Pump on."
+    GPIO.output(pumpPin, GPIO.HIGH)
+    time.sleep(duration)
+    print "pump off"
+    GPIO.output(pumpPin, GPIO.LOW)
+
+activatePump(10)
